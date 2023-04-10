a5fdc85980ae7b51dab7f9629bab9da07e476f9a
Roman Ahmad Zeia <91338897+roman-ahmadzeia@users.noreply.github.com>
2023-04-09 23:56:50 -0400

    Add files via upload

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,6 +1,7 @@
 let ws;
+var selectElement = document.getElementById("roomList");
 
-function newRoom(){
+function newRoom() {
     // calling the ChatServlet to retrieve a new room ID
     let callURL= "http://localhost:8080/WSChatServer-1.0-SNAPSHOT/chat-servlet";
     fetch(callURL, {
@@ -10,25 +11,62 @@ function newRoom(){
         },
     })
         .then(response => response.text())
-        .then(response => enterRoom(response)); // enter the room with the code
+        .then(code => {
+            console.log(code);
+            enterRoom(code); // enter the room with the code
+            selectElement.add(new Option(code));
+        });
 }
-function enterRoom(code){
 
-    // refresh the list of rooms
+function enterRoom(code) {
+    ws = new WebSocket("ws://localhost:8080/WSChatServerDemo-1.0-SNAPSHOT/ws/" + code);
+    ws.onopen = function (event)
+    {
+        document.getElementById("log").value += "[" + timestamp() + "]" + "Welcome! Please state your username:" + "\n";
+    }
 
-    // create the web socket
-    ws = new WebSocket("ws://localhost:8080/WSChatServer-1.0-SNAPSHOT/ws/"+code);
-
-
-    // parse messages received from the server and update the UI accordingly
     ws.onmessage = function (event) {
-        console.log(event.data);
-        // parsing the server's message as json
-        let message = JSON.parse(event.data);
+        let data = JSON.parse(event.data);
+        document.getElementById("log").value += "[" + timestamp() + "] " + data.message + "\n";
+    };
 
-        // handle message
 
+    document.getElementById("messageInput").addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            let request = {"type": "chat", "msg": event.target.value};
+            ws.send(JSON.stringify(request));
+            event.target.value = "";
         }
+    });
+
+    function timestamp() {
+        var d = new Date(), minutes = d.getMinutes();
+        if (minutes < 10) minutes = '0' + minutes;
+        return d.getHours() + ':' + minutes;
+    }
+}
+
+// Function to send a message to the chat room
+function sendMessage(message) {
+    if (ws) {
+        let jsonmsg = {
+            'type': 'message',
+            'message': message
+        };
+        ws.send(JSON.stringify(jsonmsg));
+    }
+}
+
+// Function to close the chat room
+function closeRoom() {
+    if (ws) {
+        ws.close();
+    }
+}
+
+function clearChat()
+{
+    document.getElementById("log").value = "";
 }
 
 
